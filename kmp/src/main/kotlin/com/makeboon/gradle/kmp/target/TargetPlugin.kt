b104f9ec7d7950cf14dcd110581035eb43d77204
ArenHansy <aren.hansy@gmail.com>
2026-07-09 00:21:39 +0900

    add AllTargets

--- kmp/src/main/kotlin/com/makeboon/gradle/kmp/target/TargetPlugin.kt
+++ kmp/src/main/kotlin/com/makeboon/gradle/kmp/target/TargetPlugin.kt
@@ -6,4 +6,5 @@ import org.gradle.api.Project
 
 public abstract class TargetPlugin : Plugin<Project>
 
+public fun AllTargets(library: Boolean): Array<TargetPlugin> = arrayOf(*MobileTargets, WebTargetPlugin(library))
 public val MobileTargets: Array<TargetPlugin> = arrayOf(AndroidTargetPlugin, AppleTargetPlugin { iosTargets })
